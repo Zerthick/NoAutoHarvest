472b2f05c4ed7be782dbe547686bbf399731ccae
Zerthick <zerthick@gmail.com>
2015-01-03 14:36:20 -0500

    Updated Version Numbers

--- src/com/gmail/zerthick/noautoharvest/NoAutoHarvestMain.java
+++ src/com/gmail/zerthick/noautoharvest/NoAutoHarvestMain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 zerthick
+ * Copyright (C) 2015 Zerthick
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
